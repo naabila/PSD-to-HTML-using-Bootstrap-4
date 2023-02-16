163b14ab277442ad1f6d9f704fa3ae84f82215df
nabila Islam <nabilaislamhstu53@gmail.com>
2023-02-16 22:55:23 +0600

    logo carousel

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
 const buttonContainer= document.querySelector('.filter-image-container');
-var mixer = mixitup(buttonContainer);
+var mixer = mixitup(buttonContainer, {
+    selectors: {
+        control: '[data-ref="mix"]'
+    }
+});
+// var mixer = mixitup(buttonContainer);
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+    
+    
+   
+    
+    
+    });
+  });
+
